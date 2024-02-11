f7361791b4f580b9914ff113eff0f24fb439bec3
chrishare10 <chrishare.png@gmail.com>
2024-02-10 21:03:10 -0500

    change to redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
                 className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
                 htmlFor="inline-full-name"
               >
-                Channel Name
+                Channel Name Here
               </label>
             </div>
             <div>
